b184edb1ad3a54b9d15733e52e26879e0d85e0d9
Jeston <jeston.lewis@protonmail.com>
2023-08-20 18:46:27 -0600

    basic test runner

--- SimpleAudioRecorder/main.cpp
+++ SimpleAudioRecorder/main.cpp
@@ -6,9 +6,13 @@
 //
 
 #include <iostream>
+#include "recorder.h"
+
+using namespace std;
 
 int main(int argc, const char* argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    Recorder recorder;
+    recorder.start();
+
     return 0;
 }
