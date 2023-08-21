f2a43be4c422f64784348c110ab64761471d4ef6
Jeston <jeston.lewis@protonmail.com>
2023-08-20 18:48:22 -0600

    added default constructor and fixed some spelling

--- SimpleAudioRecorder/wav.cpp
+++ SimpleAudioRecorder/wav.cpp
@@ -7,9 +7,8 @@
 
 #include "wav.h"
 
-Wav::Wav(string c_id, string c_s, string f, string sc1_id, int sc1_s, int a_f, int n_ch, int s_r, int b_r, int b_a, int b_p_s, string sc2_id, string sc2_s)
-	:chunk_id{ c_id }, chunk_size{ c_s }, format{ f }, subchunk1_id{ sc1_id }, subchunk1_size{ sc1_s }, audio_format{ a_f }, num_channels{ n_ch }, sample_rate{ s_r }, byte_rate{ b_r }, block_align{ b_a }, bits_per_sample{ b_p_s }, subchunk2_id{ sc2_id }, subchunk2_size{ sc2_s } {
-	chunk_id = "RIFF"; 
+Wav::Wav() {
+	chunk_id = "RIFF";
 	chunk_size = "----";
 	format = "WAVE";
 
@@ -25,6 +24,9 @@ Wav::Wav(string c_id, string c_s, string f, string sc1_id, int sc1_s, int a_f, i
 	subchunk2_id = "data";
 	subchunk2_size = "----";
 }
+
+Wav::Wav(string c_id, string c_s, string f, string sc1_id, int sc1_s, int a_f, int n_ch, int s_r, int b_r, int b_a, int b_p_s, string sc2_id, string sc2_s)
+	:chunk_id{ c_id }, chunk_size{ c_s }, format{ f }, subchunk1_id{ sc1_id }, subchunk1_size{ sc1_s }, audio_format{ a_f }, num_channels{ n_ch }, sample_rate{ s_r }, byte_rate{ b_r }, block_align{ b_a }, bits_per_sample{ b_p_s }, subchunk2_id{ sc2_id }, subchunk2_size{ sc2_s } {}
 
 Wav::Wav(const Wav &source)
 	:chunk_id{ source.chunk_id }, chunk_size{ source.chunk_size }, format{ source.format }, subchunk1_id{ source.subchunk1_id }, subchunk1_size{ source.subchunk1_size }, audio_format{ source.audio_format }, num_channels{ source.num_channels }, sample_rate{ source.sample_rate }, byte_rate{ source.byte_rate }, block_align{ source.block_align }, bits_per_sample{ source.bits_per_sample }, subchunk2_id{ source.subchunk2_id }, subchunk2_size{ source.subchunk2_size } {}
@@ -66,7 +68,7 @@ void Wav::set_byte_rate(int in) {
 void Wav::set_block_align(int in) {
 	block_align = in;
 }
-void Wav::set_bit_per_sample(int in) {
+void Wav::set_bits_per_sample(int in) {
 	bits_per_sample = in;
 }
 void Wav::set_subchunk2_id(string in) {
@@ -107,7 +109,7 @@ int Wav::get_byte_rate() {
 int Wav::get_block_align() {
 	return block_align;
 }
-int Wav::get_bit_per_sample() {
+int Wav::get_bits_per_sample() {
 	return bits_per_sample;
 }
 string Wav::get_subchunk2_id() {
--- SimpleAudioRecorder/wav.h
+++ SimpleAudioRecorder/wav.h
@@ -33,6 +33,7 @@ private:
 	string subchunk2_size;
 
 public:
+	Wav();
 	Wav(string c_id, string c_s, string f, string sc1_id, int sc1_s, int a_f, int n_ch, int s_r, int b_r, int b_a, int b_p_s, string sc2_id, string sc2_s);
 	Wav(const Wav &source);
 	~Wav();
@@ -50,7 +51,7 @@ public:
 	void set_sample_rate(int in);
 	void set_byte_rate(int in);
 	void set_block_align(int in);
-	void set_bit_per_sample(int in);
+	void set_bits_per_sample(int in);
 	void set_subchunk2_id(string in);
 	void set_subchunk2_size(string in);
 
@@ -65,7 +66,7 @@ public:
 	int get_sample_rate();
 	int get_byte_rate();
 	int get_block_align();
-	int get_bit_per_sample();
+	int get_bits_per_sample();
 	string get_subchunk2_id();
 	string get_subchunk2_size();
 };
